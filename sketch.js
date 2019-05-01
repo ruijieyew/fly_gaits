d9c00e5dc3ef1d287113fca3cbaccc1cb6745962
Rui-Jie Yew <ruijie.yew@gmail.com>
2019-05-01 03:05:59 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function draw() {
   // background(BACKGROUND);
   image(img, 0, height/3, img.width/2, img.height/2);
   if (i==3){
-    background(250);
+    background(150);
     image(img, 0, height/3, img.width/2, img.height/2);
     i = 0;
   }
